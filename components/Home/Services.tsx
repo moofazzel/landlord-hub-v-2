0032dba85b56b9a337600a3806226f425ecedd05
moofazzel <2192461016@uttarauniversity.edu.bd>
2023-04-06 22:05:45 +0600

    temporary ignoring typescript

--- components/Home/Services.tsx
+++ components/Home/Services.tsx
@@ -33,8 +33,11 @@ function Services() {
         <hr className="mt-6 mb-12 w-[50%] mx-auto border-[#4b4b4b]" />
 
         <div className=" grid gap-[30px] mb-8 md:grid-cols-2 lg:grid-cols-3">
-          {services.map((service) => (
-            <div className="text-center bg-white border rounded-[10px] p-[38px] pb-9 duration-300 transform  hover:-translate-y-2 mainShadow">
+          {services.map((service, i) => (
+            <div
+              key={i}
+              className="text-center bg-white border rounded-[10px] p-[38px] pb-9 duration-300 transform  hover:-translate-y-2 mainShadow"
+            >
               <div className="flex justify-center items-center bgIconGradient rounded-full w-28 h-28 mx-auto">
                 {service.icon}
               </div>
