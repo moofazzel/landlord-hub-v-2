ec34bbdd4b054800c8f2eb62f3b4bda405a891d3
moofazzel <2192461016@uttarauniversity.edu.bd>
2023-04-10 00:52:16 +0600

    User authentication changed to firebase and redux create user profile

--- pages/dashboard/userprofile.jsx
+++ pages/dashboard/userprofile.jsx
@@ -3,7 +3,7 @@ import { selectUser } from "@/features/userSlice";
 import { useSelector } from "react-redux";
 
 function UserProfile() {
-  const { user } = useSelector(selectUser);
+  const user = useSelector(selectUser);
   return <div>This is user profile : {user?.email}</div>;
 }
 
